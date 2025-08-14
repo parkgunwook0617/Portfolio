55436fcdef3bdd2aa91aae7d824d2f5dcae13b37
parkgunwook <ss944161269441@gmail.com>
2025-08-14 11:12:17 +0000

    [feat]: add tailwindcss setting

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
+        <p className="text-red-500 text-3xl font-bold underline">
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
