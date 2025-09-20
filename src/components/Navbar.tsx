7b84368f19b29192a5cb821e6e573c997723d29d
parkgunwook0617 <ss944161269441@gmail.com>
2025-09-20 20:33:21 +0900

    [feat]: add links (#12)

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
 import '../styling/index.css'
 
 const Navbar = () => {
@@ -26,22 +25,22 @@ const Navbar = () => {
             ${isScrolled ? 'shadow-xl/20 transition-all duration-300 ease-in-out bg-white' : 'transition-shadow duration-300 ease-in-out'}`}>
             <h1 className='font-semibold text-desert-sand'>PGW Portfolio</h1>
             <div className='flex w-md justify-between font-medium space-x-6'>
-                <Link to={'/'} className={`group relative ${isScrolled ? 'text-paynes-grey' : 'text-white'}`}>
+                <a href='#about' className={`group relative ${isScrolled ? 'text-paynes-grey' : 'text-white'}`}>
                     About
                     <span className={`absolute bottom-0 left-0 w-0 h-0.5 ${isScrolled ? 'bg-paynes-grey' : 'bg-white'} transition-all duration-300 group-hover:w-full`}></span>
-                </Link>
-                <Link to={'/'} className={`group relative ${isScrolled ? 'text-paynes-grey' : 'text-white'}`}>
+                </a>
+                <a href='#skills' className={`group relative ${isScrolled ? 'text-paynes-grey' : 'text-white'}`}>
                     Skills
                     <span className={`absolute bottom-0 left-0 w-0 h-0.5 ${isScrolled ? 'bg-paynes-grey' : 'bg-white'} transition-all duration-300 group-hover:w-full`}></span>
-                </Link>
-                <Link to={'/'} className={`group relative ${isScrolled ? 'text-paynes-grey' : 'text-white'}`}>
+                </a>
+                <a href='#projects' className={`group relative ${isScrolled ? 'text-paynes-grey' : 'text-white'}`}>
                     Projects
                     <span className={`absolute bottom-0 left-0 w-0 h-0.5 ${isScrolled ? 'bg-paynes-grey' : 'bg-white'} transition-all duration-300 group-hover:w-full`}></span>
-                </Link>
-                <Link to={'/'} className={`group relative ${isScrolled ? 'text-paynes-grey' : 'text-white'}`}>
+                </a>
+                <a href='#history' className={`group relative ${isScrolled ? 'text-paynes-grey' : 'text-white'}`}>
                     History
                     <span className={`absolute bottom-0 left-0 w-0 h-0.5 ${isScrolled ? 'bg-paynes-grey' : 'bg-white'} transition-all duration-300 group-hover:w-full`}></span>
-                </Link>
+                </a>
             </div>
         </nav>
     )
